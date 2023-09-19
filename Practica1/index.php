2e45ad066037b8493271e5eed1419ef2e61c721c
jsalgua027 <jsalcedoguarde@gmail.com>
2023-09-19 10:09:22 +0200

    terminado el formulario

--- Practica1/index.php
+++ Practica1/index.php
@@ -8,12 +8,12 @@
 </head>
 
 <body>
-    <form action="index.php" method="post" enctype="multipart/form-data">
-
+    <form action="recogida.php" method="get" enctype="multipart/form-data">
+        <!-- get envia y muestra post envia y oculta en el url para realizar pruebas se usa el get-->
 
         <h1>Rellena tu CV</h1>
         <p> <label for="nombre">Nombre </label> </br>
-            <input type="text" name="nombre" id="nombre" />
+            <input type="text" name="nombre" id="nombre" /> <!--Tengo que añadir el name para que mande el valor -->
         </p>
         <p><label for="Apellidos">Apellidos </label> </br>
             <input type="text" name="apelidos" id="apellidos" />
@@ -24,12 +24,13 @@
         <p><label for="dni">DNI </label> </br>
             <input type="text" name="dni" id="dni" />
         </p>
-        <p>Sexo</br>
-            <input type="radio" name="sexo" id="hombre" /> <label>Hombre</label></br>
-            <input type="radio" name="sexo" id="mujer" /> <label>Mujer</label>
+        <p>Sexo</br> <!-- fundamental poner el value para que se sepa el valor de la seleccion en la url-->
+            <input type="radio" name="sexo" id="hombre" value="hombre" /> <label for="hombre">Hombre</label></br>
+            <input type="radio" name="sexo" id="mujer" value="mujer" /> <label for="mujer">Mujer</label>
         </p>
-        <p> Incluir mi foto:
-            <input type="file" name="foto" id="foto">
+        <p> 
+            <label for="foto">Incluir foto</label>
+            <input type="file" name="foto" id="foto" accept="image/*"> <!--accept es para que solo acepte imagenes -->
         </p>
         <p>
             Nacido en:
