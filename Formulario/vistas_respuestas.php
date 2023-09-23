35e3f8cf19fcf0da5774899925d9ff674b4154cd
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-09-23 09:59:16 +0200

    terminado vistas_respuestas

--- Formulario/vistas_respuestas.php
+++ Formulario/vistas_respuestas.php
@@ -23,11 +23,39 @@
     // primero controlo que marque uno de los tres
     if (isset($_POST['deportes']) || isset($_POST['lectura']) || isset($_POST['otros'])) {
         echo "<p><strong>Aficiones: </strong>Has seleccionado algo.</p>";
-        //tengo que mostar 1.el que sea 2. el que sea       
+        //tengo que mostar 1.el que sea 2. el que sea   
+            // creo un array donde voy a meter los datos para despues mostrarlo por el html
+            $aficiones=[];
+            $contador=1;
+            if (isset($_POST['deportes'])) {
+               $aficiones['Deportes']=$_POST['deportes'];
+              
+            }
+            if (isset($_POST['lectura'])) {
+                $aficiones['Lectura']=$_POST['lectura'];
+               
+
+             }
+             if (isset($_POST['otros'])) {
+                $aficiones['Otros']=$_POST['otros'];
+                
+             }
+               
+             //imprimo el array 
+             foreach ($aficiones as $i => $valor) {
+                echo "$contador. $i<br>";
+                $contador++;
+            }
 
     } else{
 
-        echo "<p><strong>Aficiones: </strong>No seleccionado.</p>";
+        echo "<p><strong>No has seleccionado ninguna afición</strong>.</p>";
+    }
+    if (isset($_POST['comentarios'])) {
+       echo "<p><strong>El comentario enviado es: </strong>".$_POST['comentarios']."</p>";
+    }else{
+
+        echo "<p><strong>No has echo ningun comentario </strong></p>";
     }
 
     ?>
