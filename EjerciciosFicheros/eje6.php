a113226151c43b8f2e49c451fd7eb7a725d4b2bc
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-10-18 22:47:53 +0200

    nada

--- EjerciciosFicheros/eje6.php
+++ EjerciciosFicheros/eje6.php
@@ -6,7 +6,7 @@
     <title>Ejercicio 6</title>
 </head>
 <body>
-
+    
     <?php 
     // primero abro en modo lectura el archivo
     $fd = fopen("http://dwese.icarosproject.com/PHP/datos_ficheros.txt", "r");
@@ -16,10 +16,38 @@
         
     }
     // cojo cada linea
-    
+    $linea=fgets($fd);
+    $datos_linea=explode("\t",$linea);
+    $n_columnas=count($datos_linea);
+
+    while($linea=fgets($fd)){
+        $datos_linea1=explode(",",$linea);
+      $nu =explode(" ", $datos_linea1[2]);
+    //  $va=substr($datos_linea1[2],2);
+       
+       
+       
+        echo"<tr>";
+       echo "<th>".$nu[0]."</th></br>";
+      //  echo "<th>".$va."</th></br>";
+      //  echo "<th>".$datos_linea1[2]."</th></br>";
+        echo"</tr>";
+
+    }
+    echo"</br>";
+    echo"PRUEBA--------------------SEPARACION";
+/*   echo"<tr>";
+    for ($i=0; $i <$n_columnas ; $i++) { 
+        echo"<th>".$datos_linea[$i]."</th>";
+    }
+
+    echo "</tr>";
+       */
+   
            
     
     ?>
+    
     <form action="eje6.php" method="post" action=""></form>
 </body>
 </html>
