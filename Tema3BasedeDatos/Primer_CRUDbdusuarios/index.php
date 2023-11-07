c9867db1e486b90c08522454fc64fbd14ae60a1c
jsalgua027 <jsalcedoguarde@gmail.com>
2023-11-07 09:17:17 +0100

    continuamos

--- Tema3BasedeDatos/Primer_CRUDbdusuarios/index.php
+++ Tema3BasedeDatos/Primer_CRUDbdusuarios/index.php
@@ -1,21 +1,43 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Practica 1 CRUD</title>
 
     <style>
-        table, th, td{border: 1px solid black;}
-        table{ border-collapse: collapse; text-align: center;}
-        th{background-color: #ccc;}
-            table img{
-                width: 75px;
+        table,
+        th,
+        td {
+            border: 1px solid black;
+        }
 
-            }
+        table {
+            border-collapse: collapse;
+            text-align: center;
+        }
 
+        th {
+            background-color: #ccc;
+        }
+
+        table img {
+            width: 75px;
+
+        }
+
+        .enlace {
+
+            border: none;
+            background: none;
+            cursor: pointer;
+            color: blue;
+
+        }
     </style>
 </head>
+
 <body>
     <h1>Listado de usuarios</h1>
 
@@ -23,28 +45,27 @@
     <?php
     // REALIZO LA CONEXION
     try {
-        $conexion=mysqli_connect("localhost","jose","josefa","bd_foro");
+        $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro");
         // traduce los caracteres de la base de datos a nuestro codigo; hay que hacerlo 
-        mysqli_set_charset($conexion,"utf8");
+        mysqli_set_charset($conexion, "utf8");
     } catch (Exception $e) {
-        
+
         //si no logro conectarme
-                                                                //OJO el die termina todo y necesita un fin de body y de html
-        die("<p class='error'>No he podido conectarme a la base de datos: ".$e->getMessage()." </p></body></html>");
+        //OJO el die termina todo y necesita un fin de body y de html
+        die("<p class='error'>No he podido conectarme a la base de datos: " . $e->getMessage() . " </p></body></html>");
     }
 
     //REALIZO LA CONSULTA
     try {
-        $consulta="select * from usuarios";
-        $resultado=mysqli_query($conexion,$consulta);
-        
+        $consulta = "select * from usuarios";
+        $resultado = mysqli_query($conexion, $consulta);
     } catch (Exception $e) {
-        
-        
-                                                               
-        die("<p class='error'>No he podido realizar la consulta: ".$e->getMessage()." </p></body></html>");
-         // si falla , cierro conexión
-         mysqli_close($conexion);
+
+
+
+        die("<p class='error'>No he podido realizar la consulta: " . $e->getMessage() . " </p></body></html>");
+        // si falla , cierro conexión
+        mysqli_close($conexion);
     }
 
 
@@ -52,26 +73,76 @@
     echo "<table>";
     echo "<tr><th>Nombre Usuario</th><th>Borrar</th><th>Editar</th></tr>";
     //recorro los datos
-    while ($tupla=mysqli_fetch_assoc($resultado)) {
-        echo"<tr>";
-        echo"<td>".$tupla["nombre"]."</td>";
-        echo"<td><img src='images/borrar2.png' alt= 'Imagen borrar'></td>";
-        echo"<td><img src='images/editar2.png' alt= 'Editar borrar'></td>";
-        
-       echo"</tr>";
+    while ($tupla = mysqli_fetch_assoc($resultado)) {
+        echo "<tr>";
+        echo "<td><form action='index.php' method='post'><button class='enlace' type='submit' value='" . $tupla["id_usuario"] . "' name='btnDetalle'>" . $tupla["nombre"] . "</form></td>";
+        echo "<td><form action='index.php' method='post'><button type='submit' value='" . $tupla["id_usuario"] . "' name='btnBorrar'><img src='images/borrar2.png' alt= 'Imagen borrar'></button></form></td>";
+        echo "<td><form action='index.php' method='post'><button type='submit' value='" . $tupla["id_usuario"] . "' name='btnEditar'><img src='images/editar2.png' alt= 'Editar borrar'></button></form></td>";
+
+        echo "</tr>";
     }
     echo "</table>";
 
-    echo "<form action= 'usuario_nuevo.php'  method='post'>";
-    echo "<p><button type='submit' name='btnNuevoUsuario'>Insertar nuevo usuario</button></p>";
-    echo "</form>";
-  
+
+
+    if (isset($_POST["btnDetalle"])) {
+
+        echo "<h3>Detalles del usuario con id:  " . $_POST["btnDetalle"] . "</h3>";
+
+        //REALIZO LA CONSULTA
+        try {
+            $consulta = "select * from usuarios where id_usuario='" . $_POST["btnDetalle"] . "'";
+            $resultado = mysqli_query($conexion, $consulta);
+        } catch (Exception $e) {
+
+
+
+            die("<p class='error'>No he podido realizar la consulta: " . $e->getMessage() . " </p></body></html>");
+            // si falla , cierro conexión
+            mysqli_close($conexion);
+        }
+
+        if (mysqli_num_rows($resultado) > 0) {
+
+            $datos_usuarios = mysqli_fetch_assoc($resultado);
+
+
+            echo "<p>";
+            echo "<strong>Nombre: </strong>" . $datos_usuarios["nombre"] . "</br>";
+            echo "<strong>Usuaio: </strong>" . $datos_usuarios["usuario"] . "</br>";
+            echo "<strong>Email </strong>" . $datos_usuarios["email"] . "</br>";
+            echo "</p>";
+        } else {
+
+            echo "<p>El usuario seleccionado no se encuentra registrado en la base de datos</p>";
+        }
+
+
+        //boton para volver al index
+        echo "<form action= 'index.php'  method='post'>";
+        echo "<p><button type='submit'>Volver</button></p>";
+        echo "</form>";
+        // si le da al boton borrar
+    } elseif (isset($_POST["btnBorrar"])) {
+        # code...
+        //si le da al boton editar
+    } elseif (isset($_POST["btnEditar"])) {
+        # code...
+    } else {
+
+        echo "<form action= 'usuario_nuevo.php'  method='post'>";
+        echo "<p><button type='submit' name='btnNuevoUsuario'>Insertar nuevo usuario</button></p>";
+        echo "</form>";
+    }
+
     //libero el cache
 
     mysqli_free_result($resultado);
     //cierro conexión
-    mysqli_close($conexion)
-    
+    mysqli_close($conexion);
+
+
     ?>
 </body>
+
 </html>
---
+++ Tema3BasedeDatos/Primer_CRUDbdusuarios/usuariomio.php
@@ -0,0 +1,239 @@
+<?php
+
+function error_page($title, $body)
+{
+    $page = '<!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>' . $title . '</title>
+        </head>
+        <body>' . $body . ' </body>
+        </html>';
+    return $page;
+}
+
+function repetido($conexion, $tabla, $columna, $valor)
+{
+
+    //REALIZO LA CONSULTA
+    try{
+        $consulta="select * from ".$tabla." where ".$columna."='".$valor."'";
+        $resultado=mysqli_query($conexion, $consulta);
+        $respuesta=mysqli_num_rows($resultado)>0;
+        mysqli_free_result($resultado);
+    }
+    catch(Exception $e)
+    {
+        mysqli_close($conexion);
+        $respuesta=error_page("Práctica 1º CRUD","<h1>Práctica 1º CRUD</h1><p>No se ha podido hacer la consulta: ".$e->getMessage()."</p>");
+    }
+    return $respuesta;
+}
+
+if (isset($_POST["btnNuevoUsuario"]) || isset($_POST["btnConInsertar"])) {  //la condicion de insertar es por si hay errores al darle continuar y hay errores
+
+    //si pulso insertar controlo error del formulario de esta pagina
+
+    if (isset($_POST["btnConInsertar"])) {
+        $error_nombre = $_POST["nombre"] == "" || strlen($_POST["nombre"]) > 30;
+        $error_usuario = $_POST["usuario"] == "" || strlen($_POST["usuario"]) > 20;
+        //si no hay error en el usuario compruebo en la base de datos que no este repetido
+        if (!$error_usuario) {
+            // REALIZO LA CONEXION
+            try {
+                $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro");
+                // traduce los caracteres de la base de datos a nuestro codigo; hay que hacerlo 
+                mysqli_set_charset($conexion, "utf8");
+            } catch (Exception $e) {
+
+                //si no logro conectarme
+                //OJO el die termina todo  y le paso a mi metodo el titulo y el body
+                die(error_page("Practica 1 CURD", "<p class='error'>No he podido conectarme a la base de datos: " . $e->getMessage() . " </p>"));
+            }
+
+            $error_usuario = repetido($conexion, "usuarios", "usuario", $_POST["usuario"]);
+
+            if (is_string($error_usuario)) {
+
+                die($error_usuario);
+            }
+        }
+
+
+
+        $error_clave = $_POST["clave"] == "" || strlen($_POST["clave"]) > 15;
+        //si esta vacio o no pasa el filtro
+        $error_email = $_POST["email"] == "" || strlen($_POST["email"]) > 50 || !filter_var($_POST["email"], FILTER_VALIDATE_EMAIL);
+        if (!$error_email) {
+            //si no hay conexexion conectate
+            if (!isset($conexion)) {
+                // REALIZO LA CONEXION
+                try {
+                    $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_foro");
+                    // traduce los caracteres de la base de datos a nuestro codigo; hay que hacerlo 
+                    mysqli_set_charset($conexion, "utf8");
+                } catch (Exception $e) {
+
+                    //si no logro conectarme
+                    //OJO el die termina todo  y le paso a mi metodo el titulo y el body
+                    die(error_page("Practica 1 CURD", "<p class='error'>No he podido conectarme a la base de datos: " . $e->getMessage() . " </p>"));
+                }
+            }
+            //REALIZO LA CONSULTA
+            $error_email = repetido($conexion, "usuarios", "usuario", $_POST["email"]);
+
+            if (is_string($error_email))
+                die($error_email);
+        }
+
+        $error_form = $error_nombre || $error_usuario || $error_clave || $error_email;
+
+        if (!$error_form) {
+            //los header location hay que hacerlos antes de escibir el hmtl
+
+
+            try {
+                $consulta = "insert into usuario (nombre,usuario,clave,email) values ('" . $_POST["nombre"] . "','" . $_POST["usuario"] . "','" . md5($_POST["clave"]) . "','" . $_POST["email"] . "'))";
+                mysqli_query($conexion, $consulta);
+            } catch (Exception $e) {
+
+
+                // si falla , cierro conexión
+                mysqli_close($conexion);
+                die(error_page("Practica 1 CURD", "<p class='error'>No he podido hacer la consulta " . $e->getMessage() . " </p>"));
+            }
+
+            mysqli_close($conexion);
+
+            header("Location:index.php");
+            exit;
+        }
+        // por aqui continuo solo si hay errores en el formulario
+        if (isset($conexion)) {
+            mysqli_close($conexion);
+        }
+    }
+
+
+
+?>
+
+    <!DOCTYPE html>
+    <html lang="en">
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Practica 1 CURD</title>
+        <style>
+            .error {
+                color: red;
+
+            }
+        </style>
+    </head>
+
+    <body>
+        <h1>Nuevo Usuario</h1>
+
+        <form action="usuario_nuevo.php" method="post">
+            <p>
+                <label for="nombre">Nombre:</label>
+                <input type="text" name="nombre" id="nombre" value="<?php if (isset($_POST["nombre"])) echo $_POST["nombre"] ?>">
+                <?php
+                if (isset($_POST["btnConInsertar"]) && $error_nombre) {
+                    if ($_POST["nombre"] == "")
+                        echo "<span class='error'>Campo vacio</span>";
+                    else {
+
+                        echo "<span class='error'>Has superado los 30 caracteres</span>";
+                    }
+                }
+
+
+                ?>
+
+
+            </p>
+            <p>
+                <label for="usuario">Usuario:</label>
+                <input type="text" name="usuario" id="usuario" value="<?php if (isset($_POST["usuario"])) echo $_POST["usuario"] ?>">
+
+                <?php
+                if (isset($_POST["btnConInsertar"]) && $error_usuario) {
+                    if ($_POST["usuario"] == "")
+                        echo "<span class='error'>Campo vacio</span>";
+                    elseif (strlen($_POST["usuario"]) > 20) {
+
+                        echo "<span class='error'>Has superado los 20 caracteres</span>";
+                    } else {
+                        echo "<span class='error'> Usuario repetido</span>";
+                    }
+                }
+
+                ?>
+            </p>
+
+            <p>
+                <label for="clave">Contraseña:</label>
+                <input type="password" name="clave" maxlength="15" id="clave">
+                <?php
+                if (isset($_POST["btnContInsertar"]) && $error_clave) {
+                    if ($_POST["clave"] == "")
+                        echo "<span class='error'> Campo vacío</span>";
+                    else
+                        echo "<span class='error'> Has tecleado más de 15 caracteres</span>";
+                }
+                ?>
+            </p>
+
+            <p>
+                <label for="email">Email:</label>
+                <input type="text" name="email" id="email" value="<?php if (isset($_POST["email"])) echo $_POST["email"] ?>">
+                <?php
+                if (isset($_POST["btnConInsertar"]) && $error_email) {
+                    if ($_POST["nombre"] == "") {
+
+                        echo "<span class='error'>Campo vacio</span>";
+                    } elseif (strlen($_POST["email"]) > 50) {
+
+                        echo "<span class='error'>Has superado los 50 caracteres</span>";
+                    } elseif (!filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)) {
+                        echo "<span class='error'> Email sintáxticamente incorrecto</span>";
+                    } else {
+                        echo "<span class='error'> Email repetido</span>";
+                    }
+                }
+
+
+                ?>
+
+
+
+            </p>
+
+
+            <p>
+
+                <button type="submit" name="btnConInsertar">Continuar</button>
+                <button type="submit" name="btnVolver">Volver</button>
+            </p>
+
+
+        </form>
+
+    </body>
+
+    </html>
+
+
+<?php
+} else {
+
+    header("Location:index.php");
+    exit;
+}
+
+?>
