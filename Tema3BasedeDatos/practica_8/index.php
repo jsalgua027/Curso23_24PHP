8b6124e0ff1212dcf68d34565c852c80160277ec
jsalgua027 <jsalcedoguarde@gmail.com>
2023-11-13 13:31:03 +0100

    iniciado practica 8

---
+++ Tema3BasedeDatos/practica_8/index.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+
+//realizo la conexion
+try {
+    $conexion=mysqli_connect("localhost","jose","josefa","bd_cv");
+    mysqli_set_charset($conexion,"utf8");
+} catch (Exception $e) {
+   die("<p>No se ha podido conectarse a la base de datos: ".$e->getMessage()."</p></body></html>");
+ 
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        table,td,th{border:1px solid black}
+        table{border-collapse:collapse;text-align:center}
+        th{background-color:#CCC}
+        table img{width:50px;}
+        .enlace{border:none;background:none;cursor:pointer;color:blue;text-decoration:underline}
+        .error{color:red}  
+    </style>
+</head>
+<body>
+<h1>Práctica 8</h1>
+<?php
+// muestro la consulta
+
+require ("vistas/vista_tabla.php");
+
+
+?>
+</body>
+</html>
---
+++ Tema3BasedeDatos/practica_8/vistas/vista_tabla.php
@@ -0,0 +1,32 @@
+<?php
+// realizo la consulta
+
+try {
+    $consulta="select * from usuarios";
+    $resultado=mysqli_query($conexion, $consulta);
+
+}  catch (Exception $e) {
+    die("<p>No se ha podido hacer la consulta: ".$e->getMessage()."</p></body></html>");
+  
+ }
+ echo "<h3>Lista de Usuarios</h3>";
+
+ echo"<table>";
+ echo "<tr><th>#</th><th>Foto</th><th>Nombre</th><th><button class='enlace' type='submit' value='' name='nuevoUsu'>Usuario+</button></th></tr>";
+ while($tupla=mysqli_fetch_assoc($resultado)){
+   echo"<tr>";
+   echo"<td>".$tupla["id_usuario"]."</td>";
+   echo"<td><img src='Img/".$tupla["foto"]."' name='foto' title='foto perfil'></td>";
+   echo"<td><form action='index.php' method='post'><button class='enalce' type='submit' value='".$tupla["nombre"]."' name='btnDetalle'>".$tupla["nombre"]."</button></form></td>";
+   echo"<td><form action='index.php' method='post'><button class='enalce' type='submit' value='".$tupla["id_usuario"]."' name='btnBorrar'>Borrar</button>"." - "."
+   <button class='enalce' type='submit' value='".$tupla["id_usuario"]."' name='btnEditar'>Editar</button>
+   </form></td>";
+   echo"</tr>"; 
+
+ }
+ echo"</table>";
+ 
+
+
+
+?>
