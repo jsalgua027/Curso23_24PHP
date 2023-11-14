f20ef0d117223ec3be87496502ebfac0166e2872
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-11-14 20:13:38 +0100

    ya finciona el login XD

--- WebWorkesJava/CapítuloVJsonAJAX/ejercicios/login/login.php
+++ WebWorkesJava/CapítuloVJsonAJAX/ejercicios/login/login.php
@@ -1,18 +1,20 @@
 <?php
+
     $usuario=$_REQUEST["usuario"];
     $codigo=$_REQUEST["clave"];
 
+ 
+ function valido($a,$v){
     $usuVal="admi";
     $claveVal="1234";
-
-   if($usuario==$usuVal && $codigo==$claveVal){
-   return "USUARIO VALIDOaaaaaaa";
-
-   }else{
-   return "USUARIO NO VALIDaaaaaaO";
-   }
-    
+    if($a==$usuVal && $v==$claveVal){
+     echo"USUARIO VALIDO";
+ 
+    }else{
+    echo "USUARIO NO VALIDO";
+    }}
+   valido($usuario,$codigo);
   
-
+ 
 
 ?>
