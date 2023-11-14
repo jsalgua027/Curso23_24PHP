76c4549de43b455d7909219a98975c2eb1c186ae
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-11-14 20:33:06 +0100

    prestamo en el php

--- WebWorkesJava/CapítuloVJsonAJAX/ejercicios/Prestamo/prestamo.php
+++ WebWorkesJava/CapítuloVJsonAJAX/ejercicios/Prestamo/prestamo.php
@@ -1,6 +1,26 @@
 <?php
+$capital=$_REQUEST["capi"];
+$tasaInteres=$_REQUEST["interes"];
+$numMeses=$_REQUEST["plazos"];
+
+$num1=(int)$capital;
+$num2=(int)$tasaInteres;
+$num3=(int)$numMeses;
 
 
+function calcularCuotaMensual($cap, $int, $nMes) {
+    // Convertir la tasa de interés a términos decimales
+    $tasaInteresDecimal = $int / 100 / 12;
+
+    // Calcular la cuota mensual usando la fórmula de amortización
+    $cuotaMensual = $cap * ($tasaInteresDecimal * pow((1 + $tasaInteresDecimal), $nMes)) / (pow((1 + $tasaInteresDecimal), $nMes) - 1);
+
+    return $cuotaMensual;
+}
+
+$resultado=calcularCuotaMensual($num,$num2,$num3);
+
+echo  "$resultado";
 
 
 
