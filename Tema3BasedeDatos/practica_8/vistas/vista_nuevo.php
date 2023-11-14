b104799bb09fc296d60bc7a0c49d9f730a54f134
jsalgua027 <jsalcedoguarde@gmail.com>
2023-11-14 10:16:57 +0100

    borrado funciona, estoy con vista nuevo insercci

--- Tema3BasedeDatos/practica_8/vistas/vista_nuevo.php
+++ Tema3BasedeDatos/practica_8/vistas/vista_nuevo.php
@@ -1,4 +1,72 @@
+<?php
+    // lamo a la pagina con mis funciones 
 
+    require "src/funciones.php";
+    
+    if (isset($_POST["btnGuardar"])) {
+        
+        $error_nombre=$_POST["nombre"]=""||strlen($_POST["nombre"])>50;
+        $error_usuario=$_POST["usuario"]=""||strlen($_POST["usuario"])>30;
+         // si no hay error en el usuario compruebo que no este repetido
+        if(!$error_usuario){
+            try {
+                $conexion=mysqli_connect("localhost", "jose","josefa","bd_cv");
+                mysqli_set_charset($conexion,"utf8");
+            } catch (Exception $e) {
+                die(error_page("Práctica 8","<h1>Práctica 8 </h1><p>No he podido conectarse a la base de batos: ".$e->getMessage()."</p>"));
+            }
+            $error_usuario=repetido($conexion,"usuarios","usuario",$_POST["usuario"]);
+            
+            if(is_string($error_usuario))
+                die($error_usuario);
+
+        }
+        $error_clave = $_POST["clave"] == ""|| strlen($_POST["clave"])>50;
+        $error_dni = $_POST["dni"] == "" || !dni_bien_escrito(strtoupper($_POST["dni"])) || !dni_valido(strtoupper($_POST["dni"]));
+        // compruebo que el dni no este repetido 
+        if(!$error_dni){
+
+            try {
+                $conexion=mysqli_connect("localhost", "jose","josefa","bd_cv");
+                mysqli_set_charset($conexion,"utf8");
+            } catch (Exception $e) {
+                die(error_page("Práctica 8","<h1>Práctica 8 </h1><p>No he podido conectarse a la base de batos: ".$e->getMessage()."</p>"));
+            }
+            $error_dni=repetido($conexion,"usuarios","usuario",$_POST["dni"]);
+            
+            if(is_string($error_dni))
+                die($error_dni);
+
+
+        }
+        $error_sexo = !isset($_POST["sexo"]); // si no existe 
+        $error_archivo=  $_FILES["archivo"]["name"] != "" &&  ($_FILES["archivo"]["error"] || !getimagesize($_FILES["archivo"]["tmp_name"]) || $_FILES["archivo"]["size"] > 500 * 1024); 
+
+        $error_form=$error_nombre||$error_usuario||$error_clave||$error_dni||$error_sexo||$error_archivo;
+
+        // si no hay ningun error realizo la inserción de datos
+
+        if(!$error_form){
+           
+                try {
+                    $consulta="insert into usuarios (nombre,usuario,clave,dni,sexo) values ('".$_POST["nombre"]."','".$_POST["usuario"]."','".$_POST["clave"]."','".$_POST["dni"]."','".$_POST["sexo"]."')";
+                } catch (Exception $e) {
+                    die(error_page("Práctica 8","<h1>Práctica 8 </h1><p>No he podido conectarse a la base de batos: ".$e->getMessage()."</p>"));
+                }
+                mysqli_close($conexion);
+
+                header("Location:index.php");
+                exit;
+            // tengo que hacer el primer insert sin foto
+            // una vez que hago eso compruebo si existe el archivo foto, si existe muevo el archivo a la carpeta y si tengo exito hago un pudate con la foto (img_id)
+        }
+
+
+
+    }
+
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,7 +110,8 @@
         </p>
 
         <p>
-            <button ></button>
+            <button type="submit" name="btnGuardar">Guardar Cambios</button>
+            <button type="submit" name="atras">Atrás</button>
 
         </p>
     </form>
--- Tema3BasedeDatos/practica_8/vistas/vista_tabla.php
+++ Tema3BasedeDatos/practica_8/vistas/vista_tabla.php
@@ -1,44 +1,58 @@
 <?php
 // por si no hay conexión , la realizo
-if(!isset($conexion))
-{
-    try{
-      $conexion=mysqli_connect("localhost","jose","josefa","bd_cv");
-        mysqli_set_charset($conexion,"utf8");
-    }
-    catch(Exception $e)
-    {
-        die("<p>No ha podido conectarse a la base de batos: ".$e->getMessage()."</p></body></html>");
-    }
+if (!isset($conexion)) {
+  try {
+    $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_cv");
+    mysqli_set_charset($conexion, "utf8");
+  } catch (Exception $e) {
+    die("<p>No ha podido conectarse a la base de batos: " . $e->getMessage() . "</p></body></html>");
+  }
 }
 // realizo la consulta
 
 try {
-    $consulta="select * from usuarios";
-    $resultado=mysqli_query($conexion, $consulta);
+  $consulta = "select * from usuarios";
+  $resultado = mysqli_query($conexion, $consulta);
+} catch (Exception $e) {
+  die("<p>No se ha podido hacer la consulta: " . $e->getMessage() . "</p></body></html>");
+}
+// si le doy ha borrar
+if (isset($_POST["btnBorrar"])) {
+  //conecto
+  try {
+    $conexion = mysqli_connect("localhost", "jose", "josefa", "bd_cv");
+    mysqli_set_charset($conexion, "utf8");
+  } catch (Exception $e) {
+    die("<p>No ha podido conectarse a la base de batos: " . $e->getMessage() . "</p></body></html>");
+  }
+  // realizo la consulta borrado
+  try {
+    $consulta = "delete from usuarios where id_usuario='" . $_POST["btnBorrar"] . "'";
+    mysqli_query($conexion, $consulta);
+  } catch (Exception $e) {
+    mysqli_close($conexion);
+    die(error_page("Práctica 8", "<h1>Listado de los usuarios</h1><p>No ha podido conectarse a la base de batos: " . $e->getMessage() . "</p>"));
+  }
 
-}  catch (Exception $e) {
-    die("<p>No se ha podido hacer la consulta: ".$e->getMessage()."</p></body></html>");
-  
- }
- echo "<h3>Lista de Usuarios</h3>";
+  mysqli_close($conexion);
+  header("Location:index.php");
+  exit();
+}
 
- echo"<table>";
- echo "<tr><th>#</th><th>Foto</th><th>Nombre</th><th><form action='index.php' method='post'><button class='enlace' type='submit'  name='btnNuevoUsu'>Usuario+</button></form></th></tr>";
- while($tupla=mysqli_fetch_assoc($resultado)){
-   echo"<tr>";
-   echo"<td>".$tupla["id_usuario"]."</td>";
-   echo"<td><img src='Img/".$tupla["foto"]."' name='foto' title='foto perfil'></td>";
-   echo"<td><form action='index.php' method='post'><button class='enalce' type='submit' value='".$tupla["id_usuario"]."' name='btnDetalle'>".$tupla["nombre"]."</button></form></td>";
-   echo"<td><form action='index.php' method='post'><button class='enalce' type='submit' value='".$tupla["id_usuario"]."' name='btnBorrar'>Borrar</button>"." - "."
-   <button class='enalce' type='submit' value='".$tupla["id_usuario"]."' name='btnEditar'>Editar</button>
+
+
+echo "<h3>Lista de Usuarios</h3>";
+
+echo "<table>";
+echo "<tr><th>#</th><th>Foto</th><th>Nombre</th><th><form action='index.php' method='post'><button class='enlace' type='submit'  name='btnNuevoUsu'>Usuario+</button></form></th></tr>";
+while ($tupla = mysqli_fetch_assoc($resultado)) {
+  echo "<tr>";
+  echo "<td>" . $tupla["id_usuario"] . "</td>";
+  echo "<td><img src='Img/" . $tupla["foto"] . "' name='foto' title='foto perfil'></td>";
+  echo "<td><form action='index.php' method='post'><button class='enalce' type='submit' value='" . $tupla["id_usuario"] . "' name='btnDetalle'>" . $tupla["nombre"] . "</button></form></td>";
+  echo "<td><form action='index.php' method='post'><button class='enalce' type='submit' value='" . $tupla["id_usuario"] . "' name='btnBorrar'>Borrar</button>" . " - " . "
+   <button class='enalce' type='submit' value='" . $tupla["id_usuario"] . "' name='btnEditar'>Editar</button>
    </form></td>";
-   echo"</tr>"; 
-
- }
- echo"</table>";
- 
-
-
-
-?>
+  echo "</tr>";
+}
+echo "</table>";
