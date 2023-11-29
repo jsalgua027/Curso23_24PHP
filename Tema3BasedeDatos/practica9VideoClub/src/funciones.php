6235e40369cc0e955f674d2dd9d3356136ca4f2d
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2023-11-29 21:14:59 +0100

    peliculas

--- Tema3BasedeDatos/practica9VideoClub/src/funciones.php
+++ Tema3BasedeDatos/practica9VideoClub/src/funciones.php
@@ -1,21 +1,24 @@
 <?php
-define("SERVIDOR_BD","localhost");
-define("USUARIO_BD","jose");
-define("CLAVE_BD","josefa");
-define("NOMBRE_BD","bd_videoclub");
+// Constantes
+define("SERVIDOR_BD", "localhost");
+define("USUARIO_BD", "jose");
+define("CLAVE_BD", "josefa");
+define("NOMBRE_BD", "bd_videoclub");
 
-function error_page($title, $body)
-{
-    $page = '<!DOCTYPE html>
-    <html lang="en">
+// Página de error
+function error_page($titulo, $body) {
+    echo '
+    <!DOCTYPE html>
+    <html lang="es">
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>' . $title . '</title>
+        <title>'.$titulo.'</title>
     </head>
-    <body>' . $body . '</body>
-    </html>';
-    return $page;
+    <body>
+        '.$body.'
+    </body>
+    </html>
+    ';
 }
-
 ?>
