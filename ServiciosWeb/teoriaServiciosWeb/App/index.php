648ca037f264f900a7ca3a2fdb33b714b8c8718d
jsalgua027 <jsalcedoguarde@gmail.com>
2024-01-18 14:31:04 +0100

    terminado teooria de api (Servicios web)

--- ServiciosWeb/teoriaServiciosWeb/App/index.php
+++ ServiciosWeb/teoriaServiciosWeb/App/index.php
@@ -13,7 +13,7 @@
     /*  /*version B de metodo get versión facil*/
     define("DIR_SERV", "http://localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/ServiciosWeb/teoriaServiciosWeb/primera_api");
     //localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/ServiciosWeb/teoriaServiciosWeb/saludo--->ponemos /saludo para la url
-    
+
     /*version definitiv*/
     function consumir_servicios_REST($url, $metodo, $datos = null)
     {
@@ -40,7 +40,10 @@
     echo"<p>El saludo recibido ha sido <strong>".$obj->mensaje."</strong></p>";  
     */
     /*version definitivo*/
+
+
     $url = DIR_SERV . "/saludo";
+
     $respuesta = consumir_servicios_REST($url, "GET"); //-->indicamos la url y el tipo de metodo
     $obj = json_decode($respuesta);
     // si no encuentro respuesta muero
@@ -49,7 +52,62 @@
     }
     echo "<p>El saludo recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
 
+    $url = DIR_SERV . "/saludo/antonia"; //para usar el otro metodo get que recibe un parametro
+    $respuesta = consumir_servicios_REST($url, "GET"); //-->indicamos la url y el tipo de metodo
+    $obj = json_decode($respuesta);
+    // si no encuentro respuesta muero
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
+    }
+    echo "<p>El saludo recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
+
+    $url = DIR_SERV . "/saludo/".urlencode("Maria Antonia"); //para el tema de espacios
+    $respuesta = consumir_servicios_REST($url, "GET"); //-->indicamos la url y el tipo de metodo
+    $obj = json_decode($respuesta);
+    // si no encuentro respuesta muero
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
+    }
+    echo "<p>El saludo recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
+    /****************metodo post**************/
+    $url=DIR_SERV."/saludo";
+    $datos["nombre"]="juan alonso";
+    $respuesta = consumir_servicios_REST($url, "POST", $datos);
+    $obj = json_decode($respuesta);
+    // si no encuentro respuesta muero
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
+    }
+    echo "<p>El saludo recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
+    /*********************************DELELTE*********************/
+    $url=DIR_SERV."/borrar_saludo/37";
+   
+    $respuesta = consumir_servicios_REST($url, "DELETE");
+    $obj = json_decode($respuesta);
+    // si no encuentro respuesta muero
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
+    }
+    echo "<p>El mensaje recibido recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
+ /*********************************PUT*********************/
+    $url=DIR_SERV."/actualiar_saludo/78";
+    $datos["nombre"]="pepe perz";
+    $respuesta = consumir_servicios_REST($url, "PUT", $datos);
+    $obj = json_decode($respuesta);
+    // si no encuentro respuesta muero
+    if (!$obj) {
+        die("<p>Error consumiendo el servicio: " . $url . "<p>" . $respuesta);
+    }
+    echo "<p>El saludo recibido ha sido <strong>" . $obj->mensaje . "</strong></p>";
+
     ?>
+
+
+
 </body>
 
 </html>
--- ServiciosWeb/teoriaServiciosWeb/primera_api/index.php
+++ ServiciosWeb/teoriaServiciosWeb/primera_api/index.php
@@ -10,19 +10,47 @@ $app->run();
 require __DIR__ . '/Slim/autoload.php';
 /*localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/ServiciosWeb/teoriaServiciosWeb/saludo------------direccion y en el primer argumento se pasa el ulitmo*/
 
-$app= new \Slim\App;
+$app = new \Slim\App;
 /*el metodo get simpre recibe dos argumentos url y funcion*/
-   $app->get('/saludo',function(){
+$app->get('/saludo', function () {
 
- //siempre terminamos con un echo que es el que manda el JSON; siempre terminamos con un json de un array
- $respuesta["mensaje"]="hola";
-  echo json_encode(array("mensaje"=>"hola"));//--->OTRA FORMA DE HACER EL ARRAY
-   });
+    //siempre terminamos con un echo que es el que manda el JSON; siempre terminamos con un json de un array
+    $respuesta["mensaje"] = "hola";
+    echo json_encode(array("mensaje" => "hola")); //--->OTRA FORMA DE HACER EL ARRAY
+});
+
+// METODO GET QUE RECIBE UN PARAMETRO
+$app->get('/saludo/{nombre}', function ($request) {
+
+    $valor_recibido = $request->getAttribute('nombre');
+    $respuesta["mensaje"] = "hola " . $valor_recibido;
+    echo json_encode($respuesta);
+});
+
 // $app->post();
-// $app->delete();
-// $app->put();
 
+$app->post('/saludo', function ($request) { // metodo post que manda datos
+
+    $valor_recibido = $request->getParam('nombre'); // los datos van por abajo no por la url
+    $respuesta["mensaje"] = "hola " . $valor_recibido;
+    echo json_encode($respuesta);
+});
+
+// $app->delete();
+$app->delete('/borrar_saludo/{id}', function ($request) { // metodo post que manda datos
+
+    $id_recibida = $request->getAttribute('id'); // los datos van por abajo no por la url
+    $respuesta["mensaje"] = "Se ha borrado el saludo con id:  " . $id_recibida;
+    echo json_encode($respuesta);
+});
+// $app->put();
+$app->put('/actualiar_saludo/{id}', function ($request) { // metodo post que manda datos
+
+    $id_recibida = $request->getAttribute('id'); // los datos van por abajo no por la url
+    $nombre_nuevo = $request->getParam('nombre'); // el nombre nuevo que recivo
+    $respuesta["mensaje"] = "Se ha actualizado el saludo con id:  " . $id_recibida . " al nombre : " . $nombre_nuevo;
+    echo json_encode($respuesta);
+});
 
 
 $app->run();
-?>
