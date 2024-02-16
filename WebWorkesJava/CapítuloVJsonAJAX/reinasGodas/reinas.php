a3b0ae2c48985a5ddd123f778b50c757e1bc47d3
jsalgua027 <jsalcedoguarde@gmail.com>
2024-02-16 10:23:21 +0100

    añadido reinas de java

---
+++ WebWorkesJava/CapítuloVJsonAJAX/reinasGodas/reinas.php
@@ -0,0 +1,24 @@
+
+<?php
+$myJSON = '[
+{"nombre":"Gala Placidia","marido":"Ataúlfo"},
+{"nombre":"Flavia Valiana","marido":"Teodorico"},
+{"nombre":"Ragnahilda (Ragnachildis)","marido":"Eurico"},
+{"nombre":"Teodegonda","marido":"Alarico II"},
+{"nombre":"Clotilde (Chrodechildis)","marido":"Amalarico"},
+{"nombre":"Gosuinda (Goiswintha)","marido":"Atanagildo"},
+{"nombre":"Teodosia de Cartagena","marido":"Leovigildo"},
+{"nombre":"Goisuinta (Goiswintha)","marido":"Leovigildo"},
+{"nombre":"Ingundis","marido":"Hermenegildo"},
+{"nombre":"Baddo (Bauda)","marido":"Recaredo I"},
+{"nombre":"Clodosvinta","marido":"Recaredo I"},
+{"nombre":"Hildoara","marido":"Gundemaro"},
+{"nombre":"Teodora","marido":"Suintila"},
+{"nombre":"Riciberga","marido":"Chindasvinto"},
+{"nombre":"Liuvigoto","marido":"Ervigio"},
+{"nombre":"Cixila","marido":"Egica"},
+{"nombre":"Egilona","marido":"Don Rodrigo"}
+]';
+echo $myJSON;
+?>
+
