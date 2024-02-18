1027b0ef748060484db28555b882d50c3433a750
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-02-18 17:51:24 +0100

    pruebas con reinas

--- WebWorkesJava/CapítuloVJsonAJAX/reinasGodas/reinas.php
+++ WebWorkesJava/CapítuloVJsonAJAX/reinasGodas/reinas.php
@@ -1,25 +1,26 @@
-
 <?php
+ob_clean();
 header('Content-Type: application/json');
 $myJSON = '[
-{"nombre":"Gala Placidia","marido":"Ataúlfo"},
-{"nombre":"Flavia Valiana","marido":"Teodorico"},
-{"nombre":"Ragnahilda (Ragnachildis)","marido":"Eurico"},
-{"nombre":"Teodegonda","marido":"Alarico II"},
-{"nombre":"Clotilde (Chrodechildis)","marido":"Amalarico"},
-{"nombre":"Gosuinda (Goiswintha)","marido":"Atanagildo"},
-{"nombre":"Teodosia de Cartagena","marido":"Leovigildo"},
-{"nombre":"Goisuinta (Goiswintha)","marido":"Leovigildo"},
-{"nombre":"Ingundis","marido":"Hermenegildo"},
-{"nombre":"Baddo (Bauda)","marido":"Recaredo I"},
-{"nombre":"Clodosvinta","marido":"Recaredo I"},
-{"nombre":"Hildoara","marido":"Gundemaro"},
-{"nombre":"Teodora","marido":"Suintila"},
-{"nombre":"Riciberga","marido":"Chindasvinto"},
-{"nombre":"Liuvigoto","marido":"Ervigio"},
-{"nombre":"Cixila","marido":"Egica"},
-{"nombre":"Egilona","marido":"Don Rodrigo"}
-]';
-echo $myJSON;
+    {"nombre":"Gala Placidia","marido":"Ataúlfo"},
+    {"nombre":"Flavia Valiana","marido":"Teodorico"},
+    {"nombre":"Ragnahilda (Ragnachildis)","marido":"Eurico"},
+    {"nombre":"Teodegonda","marido":"Alarico II"},
+    {"nombre":"Clotilde (Chrodechildis)","marido":"Amalarico"},
+    {"nombre":"Gosuinda (Goiswintha)","marido":"Atanagildo"},
+    {"nombre":"Teodosia de Cartagena","marido":"Leovigildo"},
+    {"nombre":"Goisuinta (Goiswintha)","marido":"Leovigildo"},
+    {"nombre":"Ingundis","marido":"Hermenegildo"},
+    {"nombre":"Baddo (Bauda)","marido":"Recaredo I"},
+    {"nombre":"Clodosvinta","marido":"Recaredo I"},
+    {"nombre":"Hildoara","marido":"Gundemaro"},
+    {"nombre":"Teodora","marido":"Suintila"},
+    {"nombre":"Riciberga","marido":"Chindasvinto"},
+    {"nombre":"Liuvigoto","marido":"Ervigio"},
+    {"nombre":"Cixila","marido":"Egica"},
+    {"nombre":"Egilona","marido":"Don Rodrigo"}
+    ]';
+    echo $myJSON;
+    var_dump(headers_list());
 ?>
 
