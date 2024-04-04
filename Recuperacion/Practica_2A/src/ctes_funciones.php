552e8cd054f10e59f05fd1231ed2601c8f90c964
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-04-04 19:10:01 +0200

    arraglado el problema del control del tiempo de session

--- Recuperacion/Practica_2A/src/ctes_funciones.php
+++ Recuperacion/Practica_2A/src/ctes_funciones.php
@@ -6,7 +6,7 @@ define("SERVIDOR_BD","localhost");
 define("USUARIO_BD","jose");
 define("CLAVE_BD","josefa");
 define("NOMBRE_BD","bd_rec_cv");
-define("MINUTOS",20);
+define("MINUTOS",5);
 
 try {
     $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
