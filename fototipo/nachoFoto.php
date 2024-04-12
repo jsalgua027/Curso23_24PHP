8a8c6a2584bf17c972e4c98a8e0cc7e9b81714ed
jsalgua027 <jsalcedoguarde@gmail.com>
2024-04-12 10:13:57 +0200

    rngo conexion

--- fototipo/nachoFoto.php
+++ fototipo/nachoFoto.php
@@ -1,5 +1,8 @@
 <?php
-header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, PATCH");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, token, Content-Type, cache-control");
+
 $_POST=json_decode(file_get_contents("php://input"),true);
 if(!isset($_POST["nachoFoto"])){
     return;
