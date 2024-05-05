a39c0264dc8391757768ec89ca89346567eefb01
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-05-05 19:44:27 +0200

    aa

--- Recuperacion/Examen3_PHP_23_24/Libreria/admin/vistas/vista_admin.php
+++ Recuperacion/Examen3_PHP_23_24/Libreria/admin/vistas/vista_admin.php
@@ -157,31 +157,11 @@ try {
     session_destroy();
     die(error_page("Práctica Rec 2", "<h1>Práctica Rec 2</h1><p>Imposible realizar la consulta. Error:" . $e->getMessage() . "</p>"));
 }
-$usuarios = $sentencia->fetchAll(PDO::FETCH_ASSOC);
-$sentencia = null;
-
-
-
-try {
-    $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-} catch (PDOException $e) {
-    session_destroy();
-    die(error_page("Examen3 Librería", "<h1>Examen3 Librería</h1><p>Imposible conectar a la BD. Error:" . $e->getMessage() . "</p>"));
-}
-
-try {
-
-    $consulta = "SELECT * FROM libros ";
-    $sentencia = $conexion->prepare($consulta);
-    $sentencia->execute();
-} catch (PDOException $e) {
-    $sentencia = null;
-    $conexion = null;
-    session_destroy();
-    die(error_page("Examen3 Librería", "<h1>Examen3 Librería</h1><p>Imposible realizar la consulta. Error:" . $e->getMessage() . "</p>"));
-}
 $libros = $sentencia->fetchAll(PDO::FETCH_ASSOC);
 $sentencia = null;
+
+
+
 
 
 
