f6b84397dad632b3bc28de15690bcfb97f450d42
jsalgua027 <jsalcedoguarde@gmail.com>
2024-05-06 10:14:12 +0200

    añadido los metodos api2

---
+++ Recuperacion/teo_SW/segunda_api/index.php
@@ -0,0 +1,119 @@
+<?php
+
+require __DIR__ . '/Slim/autoload.php';
+$app= new \Slim\App;
+/*http://localhost/Proyectos/Curso23_24PHP/Curso23_24PHP/Recuperacion/teo_SW/primera_api/(saludo)
+primer argumento es camino (la llamada)
+hay que hacer los servicios de el ejercicio examen 3
+*/
+// metodos de logueado
+
+
+
+
+// metodo para obtener los libros
+$app->get("/obtener_libros",function(){
+
+    $respuestas["libros"]="Json con todos los libros";
+
+    echo json_encode($respuestas);
+
+});
+
+// obtener detalles libro
+
+$app->get("/detalles_libros/{referencia}",function($request){
+
+    $referencia=$request->getAttribute("referencia");
+    $respuestas["libros"]="Json con todos los  detalles libro";
+
+    echo json_encode($respuestas);
+
+});
+
+// borrar libro
+
+$app->get("/borrar_libros/{referencia}",function($request){
+    $referencia=$request->getAttribute("referencia");
+    $respuestas["mensaje"]="Libro borrado con exito";
+
+    echo json_encode($respuestas);
+
+});
+
+
+// editarlibro
+
+$app->put("/editar_libro/{referencia}",function($request){
+
+    $referencia=$request->getAttribute("referencia");
+    $titulo=$request->getParam("titulo");
+    $titulo=$request->getParam("autor");
+    //todos los demas que faltan para actuallizar
+    $respuestas["mensaje"]="Json con todos los  detalles libro";
+
+    echo json_encode($respuestas);
+
+});
+
+
+//  insertar libro
+
+$app->post("/insertar_libro/{referencia}",function($request){
+
+    $referencia=$request->getAttribute("referencia");
+    $titulo=$request->getParam("titulo");
+    $titulo=$request->getParam("autor");
+    //todos los demas que faltan 
+    
+    $respuestas["mensaje"]="Agregado el libro ";
+
+    echo json_encode($respuestas);
+
+});
+
+
+
+// repetidos
+
+$app->get("/repetido_insert/{tabla}/{columna}/{valor}",function($request){
+
+    $tabla=$request->getAttribute("tabla");
+    $columna=$request->getAttribute("columna");
+    $valor=$request->getAttribute("valor");
+    $respuestas["repetido"]="Valor repetido o no";
+
+    echo json_encode($respuestas);
+
+});
+
+
+$app->get("/repetido_edit/{tabla}/{columna}/{valor}/{columna_clave}/{valor_clave}",function($request){
+
+    $tabla=$request->getAttribute("tabla");
+    $columna=$request->getAttribute("columna");
+    $valor=$request->getAttribute("valor");
+    $columna_clave=$request->getAttribute("columna_clave");
+    $valor_clave=$request->getAttribute("valor_clave");
+    $respuestas["repetido"]="Valor repetido o no";
+
+    echo json_encode($respuestas);
+
+});
+
+// servicio actualizar para la foto
+
+$app->put("/actualizar_foto/{referencia}",function($request){
+
+    $referencia=$request->getAttribute("referencia");
+    $nombre_foto=$request->getParam("nombre_foto");
+    
+    //todos los demas que faltan para actualizar
+    $respuestas["mensaje"]="foto Actualizada";
+
+    echo json_encode($respuestas);
+
+});
+
+$app->run();
+?>
