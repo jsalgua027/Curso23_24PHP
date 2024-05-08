31c644820a5e8bd9743a3bad8223b83f64a37558
jsalgua027 <jsalcedoguarde@gmail.com>
2024-05-08 12:40:37 +0200

    modificacion

--- Restaurante/API3.php
+++ Restaurante/API3.php
@@ -17,26 +17,29 @@ try {
     
     // Recibir datos del pedido desde la aplicación React
     $data = json_decode(file_get_contents('php://input'), true);
-    var_dump($data);
+  
+    if (!isset($data['datosPedido'])){
+        exit;
+    }
+
     // Insertar un nuevo registro en la tabla de pedidos
-    $fecha = date("d-m-Y"); // Obtener la fecha actual
+    $fecha = date("Y-m-d"); // Obtener la fecha actual
     $hora = date("H:i:s"); // Obtener la hora actual
-   
     
-    $stmt = $conn->prepare("INSERT INTO nacho_pedidos (fecha, hora) VALUES (:fecha, :hora )");
+    $stmt = $pdo->prepare("INSERT INTO nacho_pedidos (fecha, hora) VALUES (:fecha, :hora )");
     $stmt->bindParam(':fecha', $fecha);
     $stmt->bindParam(':hora', $hora);
     $stmt->execute();
 
     // Obtener el ID del pedido recién insertado
-    $id_pedido =$pdo->lastInsertId();
+    $id_pedido = $pdo->lastInsertId();
 
-    // Insertar los productos y cantidades asociados con el pedido en la tabla de pedido_producto
-    foreach ($data['productos'] as $producto) {
-        $id_producto = $producto['id_producto'];
-        $cantidad = $producto['cantidad'];
+    // Insertar 
+    foreach ($data['datosPedido'] as $producto) {
+        $id_producto = $producto['id_producto']; 
+        $cantidad = $producto['cantidad']; 
 
-        $stmt = $conn->prepare("INSERT INTO nacho_pedido_producto (id_pedido, id_producto, cantidad) VALUES (:id_pedido, :id_producto, :cantidad)");
+        $stmt = $pdo->prepare("INSERT INTO nacho_pedido_producto (id_pedido, id_producto, cantidad) VALUES (:id_pedido, :id_producto, :cantidad)");
         $stmt->bindParam(':id_pedido', $id_pedido);
         $stmt->bindParam(':id_producto', $id_producto);
         $stmt->bindParam(':cantidad', $cantidad);
@@ -44,14 +47,11 @@ try {
     }
 
     // Enviar respuesta de éxito a la aplicación React
-    $response = array("status" => "success", "message" => "Pedido creado correctamente");
+    $response = array("status" => "success", "message" => "Pedido creado correctamente".$id_pedido);
     echo json_encode($response);
 } catch(PDOException $e) {
     // En caso de error, enviar respuesta de error a la aplicación React
     $response = array("status" => "error", "message" => "Error al crear el pedido: " . $e->getMessage());
     echo json_encode($response);
 }
-
-// Cerrar la conexión con la base de datos
-$conn = null;
 ?>
