3292c37eb28b2f1023d2b3f0d06683bd4cecd002
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-05-19 20:17:06 +0200

    añadido servicio_rest login en el vista_home.php

--- Recuperacion/Practica_3B_libreria/Libreria/src/seguridad.php
+++ Recuperacion/Practica_3B_libreria/Libreria/src/seguridad.php
@@ -1,41 +1,38 @@
 <?php
-//conexión
-try{
-    $conexion=new PDO("mysql:host=".SERVIDOR_BD.";dbname=".NOMBRE_BD,USUARIO_BD,CLAVE_BD,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'")); 
-}
-catch(PDOException $e){
-    session_destroy();
-    die(error_page("Práctica Rec 2","<h1>Práctica Rec 2</h1><p>Imposible conectar a la BD. Error:".$e->getMessage()."</p>"));
-}
-
-// compruebo letor
-try{
-    $datos[0]=$_SESSION["usuario"];
-    $datos[1]=$_SESSION["clave"];
-    $consulta = "SELECT * FROM usuarios WHERE lector=? AND clave=?";
-    $sentencia=$conexion->prepare($consulta);
-    $sentencia->execute($datos);
-}
-catch(PDOException $e){
-    $sentencia=null;
-    $conexion=null;
-    session_destroy();
-    die(error_page("Práctica Rec 2","<h1>Práctica Rec 2</h1><p>Imposible realizar la consulta. Error:".$e->getMessage()."</p>"));
-}
-
-if($sentencia->rowCount()<=0)
+$datos_env["api_key"]=$_SESSION["api_key"];
+$respuesta=consumir_servicios_REST(DIR_SERV."/logueado","POST",$datos_env);
+$json=json_decode($respuesta,true);
+if(!$json)
 {
-        $sentencia=null;
-        $conexion=null;
-        session_unset();
-        $_SESSION["seguridad"]="Usted ya no se encuentra registrado en la BD";
-        header("Location:".$salto);
-        exit();
+    session_destroy();
+    die(error_page("Práctica Rec 3","<h1>Práctica Rec 3</h1><p>Sin respuesta oportuna de la API</p>"));  
+}
+if(isset($json["error_bd"]))
+{
+
+    session_destroy();
+    consumir_servicios_REST(DIR_SERV."/salir","POST",$datos_env);
+    die(error_page("Práctica Rec 3","<h1>Práctica Rec 3</h1><p>".$json["error_bd"]."</p>"));
 }
 
+if(isset($json["no_auth"]))
+{
+   session_unset();
+   $_SESSION["seguridad"]="Usted ha dejado de tener acceso a la API. Por favor vuelva a loguearse.";
+   header("Location:index.php");
+   exit();
+}
+
+if(isset($json["mensaje"]))
+{
+   session_unset();
+   consumir_servicios_REST(DIR_SERV."/salir","POST",$datos_env);
+   $_SESSION["seguridad"]="Usted ya no se encuentra registrado en la BD";
+   header("Location:index.php");
+   exit();
+}
 // Acabo de pasar el control de baneo
-$datos_usuario_log=$sentencia->fetch(PDO::FETCH_ASSOC);
-$sentencia=null;
+$datos_usuario_log=$json["usuario"];
 
 if(time()-$_SESSION["ultm_accion"]>MINUTOS*60)
 {
--- Recuperacion/Practica_3B_libreria/Libreria/vistas/vista_home.php
+++ Recuperacion/Practica_3B_libreria/Libreria/vistas/vista_home.php
@@ -4,49 +4,39 @@ if (isset($_POST["btnEntrar"])) {
     $error_usuario = $_POST["usuario"] == "";
     $error_clave = $_POST["clave"] == "";
     $error_form = $error_usuario || $error_clave;
-    if (!$error_form) {
-        try {
-            $conexion = new PDO("mysql:host=" . SERVIDOR_BD . ";dbname=" . NOMBRE_BD, USUARIO_BD, CLAVE_BD, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-        } catch (PDOException $e) {
+    if(!$error_form)
+    {
+        $datos_env["usuario"]=$_POST["usuario"];
+        $datos_env["clave"]=md5($_POST["clave"]);
+        $respuesta=consumir_servicios_REST(DIR_SERV."/login","POST",$datos_env);
+        $json=json_decode($respuesta,true);
+        if(!$json)
+        {
             session_destroy();
-            die(error_page("Examen3 Librería", "<h1>Examen3 Librería</h1><p>Imposible conectar a la BD. Error:" . $e->getMessage() . "</p>"));
+            die(error_page("Práctica Rec 3B","<h1>Práctica Rec 3</h1><p>Sin respuesta oportuna de la API</p>"));  
+        }
+        if(isset($json["error_bd"]))
+        {
+            session_destroy();
+            die(error_page("Práctica Rec 3N","<h1>Práctica Rec 3</h1><p>".$json["error_bd"]."</p>"));
         }
 
-        try {
-            $datos[0] = $_POST["usuario"];
-            $datos[1] = md5($_POST["clave"]);
-            $consulta = "SELECT * FROM usuarios WHERE lector=? AND clave=?";
-            $sentencia = $conexion->prepare($consulta);
-            $sentencia->execute($datos);
-        } catch (PDOException $e) {
-            $sentencia = null;
-            $conexion = null;
-            session_destroy();
-            die(error_page("Examen3 Librería", "<h1>Examen3 Librería</h1><p>Imposible realizar la consulta. Error:" . $e->getMessage() . "</p>"));
-        }
+        if(isset($json["usuario"]))
+        {
+            $_SESSION["usuario"]=$json["usuario"]["usuario"];
+            $_SESSION["clave"]=$json["usuario"]["clave"];
+            $_SESSION["ultm_accion"]=time();
+            $_SESSION["api_key"]=$json["api_key"];
 
-        if ($sentencia->rowCount() > 0) {
-
-            $datos_usu = $sentencia->fetch(PDO::FETCH_ASSOC);
-            $sentencia = null;
-            $conexion = null;
-            $_SESSION["usuario"] = $datos[0];
-            $_SESSION["clave"] = $datos[1];
-            $_SESSION["ultm_accion"] = time();
-            if ($datos_usu["tipo"] == "normal") {
-                header("Location:index.php");
-            } else {
-                header("Location:admin/gest_libros.php");
-            }
-
+            header("Location:index.php");
             exit();
-        } else {
-            $sentencia = null;
-            $conexion = null;
-            $error_usuario = true;
         }
+        else
+            $error_usuario=true;
+
     }
 }
+
 
 
 /*****consulta para mostra la tabla******/
