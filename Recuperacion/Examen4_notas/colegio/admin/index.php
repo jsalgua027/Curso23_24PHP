9aa90405e2ea5b718d801598d45ee3341d3b209a
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-06-02 00:16:42 +0200

    boton editar

--- Recuperacion/Examen4_notas/colegio/admin/index.php
+++ Recuperacion/Examen4_notas/colegio/admin/index.php
@@ -63,7 +63,31 @@ if (isset($_POST["btnVerNotas"])) {
     if (isset($json["notas"]) && count($json["notas"]) > 0) {
         $datos_notas_alum = $json["notas"];
     }
-    
+
+    $respuesta = consumir_servicios_REST(DIR_SERV . "/NotasNoEvalAlumno/" . $cod_usu, "GET", $datos_env);
+
+    $jsonNo = json_decode($respuesta, true);
+    if (!$jsonNo) {
+        session_destroy();
+        die(error_page("Examen 4 Notas", "<h1>Examen 4 Notas</h1><p>Sin respuesta oportuna de la API asignaturas Alumno no evaluadas</p>"));
+    }
+    if (isset($jsonNo["error"])) {
+
+        session_destroy();
+        consumir_servicios_REST(DIR_SERV . "/salir", "POST", $datos_env);
+        die(error_page("Examen 4 Notas", "<h1>Examen 4 Notas</h1><p>" . $jsonNo["error"] . "</p>"));
+    }
+
+    if (isset($jsonNo["no_auth"])) {
+        session_unset();
+        $_SESSION["seguridad"] = "Usted ha dejado de tener acceso a la API. Por favor vuelva a loguearse.";
+        header("Location:index.php");
+        exit();
+    }
+
+    if (isset($jsonNo["notas"]) && count($jsonNo["notas"]) > 0) {
+        $datos_Asig_NO_Evau =  $jsonNo["notas"]; // asignaturas no evaluadas
+    }
 }
 
 
@@ -133,26 +157,48 @@ if (isset($_POST["btnVerNotas"])) {
             </p>
         </form>
         <?php
-        if (isset($_POST["btnVerNotas"])) {
+        if (isset($_POST["btnVerNotas"]) || isset($_POST["btnEditar"])) { // por aqui lo dejamos
 
             echo "<h2>Notas del Alumno " . $nombre_alumno_seleccionado . "</h2>";
             echo "<table class='table'>";
             echo "<tr><th>Asignaturas</th><th>Notas</th><th>Accion</th></tr>";
-          if(count($datos_notas_alum) > 0)
-          {
-            foreach ($datos_notas_alum as $tupla) {
-                echo "<tr>";
-                echo "<td>" . $tupla["denominacion"] . "</td>";
-                echo "<td>" . $tupla["nota"] . "</td>";
-                echo "<tr>";
+            if (count($datos_notas_alum) > 0)
+             {
+                foreach ($datos_notas_alum as $tupla) {
+                    echo "<tr>";
+                    echo "<td>" . $tupla["denominacion"] . "</td>";
+                    echo "<td>" . $tupla["nota"] . "</td>";
+                    echo "<td><form action='index.php' mehod='post'>";
+                   // echo "<input type='hidden' name='cod_asig' value='" . $tupla["cod_asig"] . "'>";
+                   // echo "<input type='hidden' name='alumnoSeleccionado' value='" . $cod_usu . "'>";
+                    echo"<button class='enlace' name='btnEditar' type='submit'>Editar</button>-<button class='enlace' name='btnBorrar' type='submit'>Borrar</button>";
+                    if(isset($_POST["btnEditar"]))
+                    {
+                        echo"<button class='enlace' name='btnCambiar' type='submit'>Cambiar</button>-<button class='enlace' name='btnAtras' type='submit'>Atras</button>";  
+                    }
+                    echo"</form></td>";
+                    echo "<tr>";
+                }
             }
-          }
-          else
-          {
-            echo"<p>NO TIENEN ASIGANURAS CALIFICADAS</p>";
-          }
-            
             echo "</table>";
+            if (count($datos_notas_alum) == 3)
+            {
+                echo "<p>A " . $nombre_alumno_seleccionado . " no quedan asignaturas por calificar</p>";
+            }
+            if (count($datos_notas_alum) < 3) 
+            {
+                echo "<p>";
+                echo "<form action='index.php' method='post'>";
+                echo "<label for='asignaturasNO'>Asignaturas que a <strong> " . $nombre_alumno_seleccionado . " </strong> aún le queda por calificar </label>";
+                echo "<select for='asignaturasNO' name='asignaturasNO'>";
+                foreach ($datos_Asig_NO_Evau as $tupla) {
+                    echo "<option value='" . $tupla["cod_asig"] . "'>" . $tupla["denominacion"] . "</option>";
+                }
+                echo "</select>";
+                echo " <button name='btnCalificar' type='submit'>Calificar</button>";
+                echo "</form>";
+                echo "</p>";
+            }
         }
 
 
