facdc3f0cac5f2f73503906dbc363f341aaff7e2
jsalgua027 <92090934+jsalgua027@users.noreply.github.com>
2024-06-12 08:54:03 +0200

    quitado comentario

--- Recuperacion/Examen4_SW/Colegio/src/seguridad.php
+++ Recuperacion/Examen4_SW/Colegio/src/seguridad.php
@@ -1,4 +1,4 @@
-$_SESSION["usuario"]=$_SESSION["usuaro"]["usuario"];<?php
+<?php
 
 $datos_env["api_session"]=$_SESSION["api_session"];
 $respuesta=consumir_servicios_REST(DIR_SERV."/logueado","GET",$datos_env);
